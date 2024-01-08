ffbefa47f5edfb48f339517c8c85e5897f58cf50
Prakash Chakraborty <prakashchakraborty09@gmail.com>
2024-01-08 23:23:28 +0000

    Code added

---
+++ prod/model.py
@@ -0,0 +1,168 @@
+import numpy as np
+import pandas as pd
+import argparse
+import mlflow
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--trainingdata", type=str, required=True, help='Dataset for training')
+parser.add_argument("--testingdata", type=str, required=True, help='Dataset for testing')
+args = parser.parse_args()
+mlflow.autolog()
+
+train = pd.read_csv(args.trainingdata)
+test = pd.read_csv(args.testingdata)
+
+columns = train.columns
+
+# Removing '()' from column names
+columns = columns.str.replace('[()]','')
+columns = columns.str.replace('[-]', '')
+columns = columns.str.replace('[,]','')
+
+train.columns = columns
+test.columns = columns
+
+y_train = train.Activity
+X_train = train.drop(['subject', 'Activity'], axis=1)
+y_test = test.Activity
+X_test = test.drop(['subject', 'Activity'], axis=1)
+print('Training data size : ', X_train.shape)
+print('Test data size : ', X_test.shape)
+
+labels=['LAYING', 'SITTING','STANDING','WALKING','WALKING_DOWNSTAIRS','WALKING_UPSTAIRS']
+
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import RandomizedSearchCV
+plt.rcParams["font.family"] = 'DejaVu Sans'
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+from datetime import datetime
+def perform_model(model, X_train, y_train, X_test, y_test, class_labels, cm_normalize=True, \
+                 print_cm=True, cm_cmap=plt.cm.Greens):
+    
+    
+    # to store results at various phases
+    results = dict()
+    
+    # time at which model starts training 
+    train_start_time = datetime.now()
+    print('training the model..')
+    model.fit(X_train, y_train)
+    print('Done \n \n')
+    train_end_time = datetime.now()
+    results['training_time'] =  train_end_time - train_start_time
+    print('training_time(HH:MM:SS.ms) - {}\n\n'.format(results['training_time']))
+    
+    
+    # predict test data
+    print('Predicting test data')
+    test_start_time = datetime.now()
+    y_pred = model.predict(X_test)
+    test_end_time = datetime.now()
+    print('Done \n \n')
+    results['testing_time'] = test_end_time - test_start_time
+    print('testing time(HH:MM:SS:ms) - {}\n\n'.format(results['testing_time']))
+    results['predicted'] = y_pred
+   
+
+    # calculate overall accuracty of the model
+    accuracy = metrics.accuracy_score(y_true=y_test, y_pred=y_pred)
+    # store accuracy in results
+    results['accuracy'] = accuracy
+    print('---------------------')
+    print('|      Accuracy      |')
+    print('---------------------')
+    print('\n    {}\n\n'.format(accuracy))
+    
+    
+    # confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_pred)
+    results['confusion_matrix'] = cm
+    if print_cm: 
+        print('--------------------')
+        print('| Confusion Matrix |')
+        print('--------------------')
+        print('\n {}'.format(cm))
+        
+    # plot confusin matrix
+    plt.figure(figsize=(8,8))
+    plt.grid(False)
+    plot_confusion_matrix(cm, classes=class_labels, normalize=True, title='Normalized confusion matrix', cmap = cm_cmap)
+    plt.show()
+    
+    # get classification report
+    print('-------------------------')
+    print('| Classifiction Report |')
+    print('-------------------------')
+    classification_report = metrics.classification_report(y_test, y_pred)
+    # store report in results
+    results['classification_report'] = classification_report
+    print(classification_report)
+    
+    # add the trained  model to the results
+    results['model'] = model
+    
+    return results
+    
+def print_grid_search_attributes(model):
+    # Estimator that gave highest score among all the estimators formed in GridSearch
+    print('--------------------------')
+    print('|      Best Estimator     |')
+    print('--------------------------')
+    print('\n\t{}\n'.format(model.best_estimator_))
+
+
+    # parameters that gave best results while performing grid search
+    print('--------------------------')
+    print('|     Best parameters     |')
+    print('--------------------------')
+    print('\tParameters of best estimator : \n\n\t{}\n'.format(model.best_params_))
+
+
+    #  number of cross validation splits
+    print('---------------------------------')
+    print('|   No of CrossValidation sets   |')
+    print('--------------------------------')
+    print('\n\tTotal numbre of cross validation sets: {}\n'.format(model.n_splits_))
+
+
+    # Average cross validated score of the best estimator, from the Grid Search 
+    print('--------------------------')
+    print('|        Best Score       |')
+    print('--------------------------')
+    print('\n\tAverage Cross Validate scores of best estimator : \n\n\t{}\n'.format(model.best_score_))
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+parameters = {'max_depth':np.arange(3,10,2)}
+dt = DecisionTreeClassifier()
+dt_grid = GridSearchCV(dt,param_grid=parameters, n_jobs=-1)
+dt_grid_results = perform_model(dt_grid, X_train, y_train, X_test, y_test, class_labels=labels)
+print_grid_search_attributes(dt_grid_results['model'])
